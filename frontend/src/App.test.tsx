7fce9ef7b2c7c3618740dbf9fed74ab2f17bac4d
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-03-12 18:47:50 +0000

    chore: Setup frontend testing

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,13 @@
+describe('Testing...', () => {
+	it('Test 1', () => {
+		expect(true).toBe(true);
+	});
+
+	it('Test 2', () => {
+		expect(false).toBe(false);
+	});
+
+	it('Test 3', () => {
+		expect(false).toBe(false);
+	});
+});
