46caf5469311c4943bc2babd171317600b3b626d
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-03-14 10:05:42 +0000

    chore: Update CD workflow

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-	res.status(200).send('Hello world 4');
+	res.status(200).send('Hello world 5');
 });
 
 export default app;
