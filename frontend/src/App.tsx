8629685d4980d9601cfa5dbcbd894231ab6ba3a7
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-03-14 10:39:11 +0000

    feat: Make the text black

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
 	return (
 		<>
 			<div>
-				<h1 className='text-2xl/7 font-bold text-white sm:truncate sm:text-3xl sm:tracking-tight'>
+				<h1 className='text-2xl/7 font-bold sm:truncate sm:text-3xl sm:tracking-tight'>
 					Snitch
 				</h1>
 				<p>Don't miss the next bus 🚌</p>
