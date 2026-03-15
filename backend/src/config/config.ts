9bb92e1569064af0b3fce6cc3c28553e08356bb6
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-03-15 22:23:30 +0000

    feat: Add the jwtsecret environment variable

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -5,12 +5,14 @@ interface Config {
 	port: number;
 	nodeEnv: string;
 	databaseUrl: string;
+	jwtSecret: string;
 }
 
 const config: Config = {
 	port: Number(process.env.PORT) || 3000,
-	nodeEnv: process.env.NODE_ENV || 'development',
-	databaseUrl: process.env.DATABASE_URL || 'MISSING DATABASE URL',
+	nodeEnv: process.env.NODE_ENV ?? 'development',
+	databaseUrl: process.env.DATABASE_URL ?? 'MISSING DATABASE URL',
+	jwtSecret: process.env.JWT_SECRET ?? 'secret',
 };
 
 export default config;
