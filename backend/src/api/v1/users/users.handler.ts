ccbe48757b20233462cd6587bdc8e0d974fc7d2c
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-03-18 09:10:56 +0000

    feat: Update test

--- backend/src/api/v1/users/users.handler.ts
+++ backend/src/api/v1/users/users.handler.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from 'express';
 
 export function getUsers(req: Request, res: Response) {
-	res.send(200).json({ message: 'Users' });
+	res.status(200).send({ message: 'Users' });
 }
--- backend/src/testing/users.test.ts
+++ backend/src/testing/users.test.ts
@@ -1,9 +1,14 @@
 import app from '../app/app.ts';
 import request from 'supertest';
 
+interface body {
+	message: string;
+}
+
 describe('User Handler', () => {
-	it('GET /users should return 200 status', async () => {
+	it('GET /users should return the message "Users"', async () => {
 		const response = await request(app).get('/api/v1/users');
-		expect(response.status).toBe(200);
+		const body = response.body as body;
+		expect(body.message).toEqual('Users');
 	});
 });
