84492a54ece649f71cb8acc731934f6f2a22a657
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-03 02:46:48 +0000

    feat: Add cors and urlencoded middlewares

--- backend/src/app/app.ts
+++ backend/src/app/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import type { Response, Request } from 'express';
 import userRoutes from '../api/v1/users/users.route.ts';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', (req: Request, res: Response) => {
 	res.status(200).send('Hello world 7');
