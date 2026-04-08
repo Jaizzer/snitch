0c3b4f9a0e67eac95b3a712fa9d9052cb21a3eb4
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-08 10:13:01 +0000

    feat: Add request structure validation

--- backend/src/api/v1/users/users.handler.ts
+++ backend/src/api/v1/users/users.handler.ts
@@ -1,5 +1,15 @@
 import type { Request, Response } from 'express';
+import { z } from 'zod';
+
+const UserInfoSchema = z.object({
+	email: z.string(),
+	password: z.string(),
+});
 
 export function createUser(req: Request, res: Response) {
+	const userInfo = UserInfoSchema.safeParse(req.body);
+	if (!userInfo.success) {
+		res.status(400).json({ message: '' });
+	}
 	res.status(201).json({ message: 'Account created successfully!' });
 }
--- backend/src/api/v1/users/users.test.ts
+++ backend/src/api/v1/users/users.test.ts
@@ -8,20 +8,49 @@ const ResponseBody = z.object({
 
 describe('Users', () => {
 	it('POST /api/v1/users should return 201 status code', async () => {
-		const response = await request(app).post('/api/v1/users');
+		const userInformation = {
+			email: 'reznov@viktor.com',
+			password: 'nBEA~0>2/ar5',
+		};
+
+		const response = await request(app)
+			.post('/api/v1/users')
+			.send(userInformation);
 		expect(response.status).toBe(201);
 	});
 
 	it('POST /api/v1/users should return a JSON response format', async () => {
-		const response = await request(app).post('/api/v1/users');
+		const userInformation = {
+			email: 'reznov@viktor.com',
+			password: 'nBEA~0>2/ar5',
+		};
+
+		const response = await request(app)
+			.post('/api/v1/users')
+			.send(userInformation);
+
 		expect(response.headers['content-type']).toBe(
 			'application/json; charset=utf-8',
 		);
 	});
 
 	it('POST /api/v1/users should return an object with message property', async () => {
-		const response = await request(app).post('/api/v1/users');
+		const userInformation = {
+			email: 'reznov@viktor.com',
+			password: 'nBEA~0>2/ar5',
+		};
+
+		const response = await request(app)
+			.post('/api/v1/users')
+			.send(userInformation);
+
 		const parsedData = ResponseBody.safeParse(response.body);
+
 		expect(parsedData.success).toBe(true);
+	});
+
+	it('POST /api/v1/users should return 400 status code for empty requests', async () => {
+		const response = await request(app).post('/api/v1/users');
+		expect(response.status).toBe(400);
 	});
 });
