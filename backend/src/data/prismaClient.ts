a785c34dddbbf10d48f072e7edfb32fe72792945
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-09 17:59:50 +0000

    ref: Rename prisma client file

--- backend/src/data/prismaClient.ts
+++ backend/src/data/prismaClient.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from './generated/prisma/client';
+import { PrismaClient } from './prisma/generated/client';
 import config from '../config/config';
 
 const connectionString = config.databaseUrl;
