2276d36c4b2bc0dd59f206eb5b08a9c13917c04a
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-09 19:17:51 +0000

    ref: Rename test for better readability

--- backend/src/api/v1/users/users.test.ts
+++ backend/src/api/v1/users/users.test.ts
@@ -7,129 +7,131 @@ const ResponseBody = z.object({
 });
 
 describe('Users', () => {
-	it('POST /api/v1/users should return 201 status code', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'nBEA~0>2/ar5',
-		};
+	describe('POST /api/v1/users', () => {
+		it('returns 201 status code for valid inputs', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'nBEA~0>2/ar5',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.status).toBe(201);
-	});
+			expect(response.status).toBe(201);
+		});
 
-	it('POST /api/v1/users should return a JSON response format', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'nBEA~0>2/ar5',
-		};
+		it('returns a JSON response format', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'nBEA~0>2/ar5',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.headers['content-type']).toBe(
-			'application/json; charset=utf-8',
-		);
-	});
+			expect(response.headers['content-type']).toBe(
+				'application/json; charset=utf-8',
+			);
+		});
 
-	it('POST /api/v1/users should return an object with message property', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'nBEA~0>2/ar5',
-		};
+		it('returns an object with message property for valid inputs', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'nBEA~0>2/ar5',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
-		const parsedData = ResponseBody.safeParse(response.body);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
+			const parsedData = ResponseBody.safeParse(response.body);
 
-		expect(parsedData.success).toBe(true);
-	});
+			expect(parsedData.success).toBe(true);
+		});
 
-	it('POST /api/v1/users should return 400 status code for empty requests', async () => {
-		const response = await request(app).post('/api/v1/users');
+		it('returns a 400 status code for empty requests', async () => {
+			const response = await request(app).post('/api/v1/users');
 
-		expect(response.status).toBe(400);
-	});
+			expect(response.status).toBe(400);
+		});
 
-	it('POST /api/v1/users should return a 400 status for passwords less than 8 characters', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'pass',
-		};
+		it('returns a 400 status for passwords less than 8 characters', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'pass',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.status).toBe(400);
-	});
+			expect(response.status).toBe(400);
+		});
 
-	it('POST /api/v1/users should return a 400 status for passwords without any digit', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'passwordpassword',
-		};
+		it('returns a 400 status for passwords without any digit', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'passwordpassword',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.status).toBe(400);
-	});
+			expect(response.status).toBe(400);
+		});
 
-	it('POST /api/v1/users should return a 400 status for passwords without any upper case', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'passwordpassword1',
-		};
+		it('returns a 400 status for passwords without any upper case', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'passwordpassword1',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.status).toBe(400);
-	});
+			expect(response.status).toBe(400);
+		});
 
-	it('POST /api/v1/users should return a 400 status for passwords without any lower case', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'PASSWORDPASSWORD1',
-		};
+		it('returns a 400 status for passwords without any lower case', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'PASSWORDPASSWORD1',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.status).toBe(400);
-	});
+			expect(response.status).toBe(400);
+		});
 
-	it('POST /api/v1/users should return a 400 status for passwords with white spaces', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: ' PASSWORD pASSWO RD1',
-		};
+		it('returns a 400 status for passwords with white spaces', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: ' PASSWORD pASSWO RD1',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.status).toBe(400);
-	});
+			expect(response.status).toBe(400);
+		});
 
-	it('POST /api/v1/users should return a 400 status for passwords without any symbol', async () => {
-		const userInformation = {
-			email: 'reznov@viktor.com',
-			password: 'PASSWORDpASSWORD1',
-		};
+		it('returns a 400 status for passwords without any symbol', async () => {
+			const userInformation = {
+				email: 'reznov@viktor.com',
+				password: 'PASSWORDpASSWORD1',
+			};
 
-		const response = await request(app)
-			.post('/api/v1/users')
-			.send(userInformation);
+			const response = await request(app)
+				.post('/api/v1/users')
+				.send(userInformation);
 
-		expect(response.status).toBe(400);
+			expect(response.status).toBe(400);
+		});
 	});
 });
