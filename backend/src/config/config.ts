0937b4f8282c7c35f9fb45fae8ecacc511897f8b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-09 18:10:34 +0000

    ref: Remove interface to utilize typescript type inference

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,14 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: import.meta.dirname + '/../../.env' });
 
-interface Config {
-	port: number;
-	nodeEnv: string;
-	databaseUrl: string;
-	jwtSecret: string;
-}
-
-const config: Config = {
+const config = {
 	port: Number(process.env.PORT) || 3000,
 	nodeEnv: process.env.NODE_ENV ?? 'development',
 	databaseUrl: process.env.DATABASE_URL ?? 'MISSING DATABASE URL',
