63fe2b4156a4256d7959f6e7adfe57fb2bf8d23a
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-11 19:48:23 +0000

    ref: Clean up app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,7 @@ import express from 'express';
 import type { Response, Request } from 'express';
 import userRoutes from './api/v1/users/users.route.ts';
 import cors from 'cors';
-import config from './config/env.ts';
 
-console.log(config);
 const app = express();
 
 app.use(cors());
@@ -13,10 +11,6 @@ app.use(express.urlencoded());
 
 app.get('/', (req: Request, res: Response) => {
 	res.status(200).send('Hello world 7');
-});
-
-app.get('/tets', (req: Request, res: Response) => {
-	res.status(200).send('test 1');
 });
 
 app.use('/api/v1/users', userRoutes);
