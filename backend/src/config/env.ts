36fa8ed6784c17370e504a290417c42fe0e31a33
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-11 04:14:46 +0000

    feat: Add condition which database to use depending on the ndoe environment

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,10 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: import.meta.dirname + '/../../.env' });
 
+const nodeEnv = process.env.NODE_ENV ?? 'development';
+
+let databaseUrl;
+if (nodeEnv === 'production') {
+	databaseUrl =
+		process.env.PRODUCTION_DATABASE_URL ??
+		'Missing Production Database URL';
+} else if (nodeEnv === 'development') {
+	databaseUrl =
+		process.env.DEVELOPMENT_DATABASE_URL ??
+		'Missing Development Database URL';
+} else {
+	databaseUrl = process.env.TEST_DATABASE_URL ?? 'Missing Test Database URL';
+}
+
 const config = {
 	port: Number(process.env.PORT) || 3000,
-	nodeEnv: process.env.NODE_ENV ?? 'development',
-	databaseUrl: process.env.DATABASE_URL ?? 'MISSING DATABASE URL',
+	nodeEnv: nodeEnv,
+	databaseUrl: databaseUrl,
 	jwtSecret: process.env.JWT_SECRET ?? 'secret',
 };
 
