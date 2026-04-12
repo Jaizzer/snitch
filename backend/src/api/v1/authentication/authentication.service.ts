406e8ad31452db7bbf3d9d2cfc4b6103a1fb32e6
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-12 18:25:56 +0000

    ref: Move the jwt methods to the shared utils directory

--- backend/src/api/v1/authentication/authentication.service.ts
+++ backend/src/api/v1/authentication/authentication.service.ts
@@ -1,17 +1,5 @@
-import jwt, { type JwtPayload } from 'jsonwebtoken';
 import { prisma } from '../../../database/prismaClient.ts';
 import passwordManager from '../../../utils/passwordManager.ts';
-
-export function generateToken(payload: string, jwtSecret: string): string {
-	return jwt.sign(payload, jwtSecret, { expiresIn: '1h' });
-}
-
-export function verifyToken(
-	token: string,
-	jwtSecret: string,
-): string | JwtPayload {
-	return jwt.verify(token, jwtSecret);
-}
 
 async function isLoginCredentialsValid({
 	email,
---
+++ backend/src/utils/jwtManager.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import env from '../config/env.ts';
+
+interface Payload {
+	id: number;
+	email: string;
+}
+
+function generateAuthorizationToken(payload: Payload) {
+	const token = jwt.sign(payload, env.jwtSecret, { expiresIn: '1m' });
+	return token;
+}
+
+function decodeToken(token: string) {
+	return jwt.verify(token, env.jwtSecret);
+}
+
+export default { generateAuthorizationToken, decodeToken };
