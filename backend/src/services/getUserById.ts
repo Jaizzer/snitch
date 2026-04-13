c058a1c71ef849b99d48c987432cef48548d8b2e
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-04-13 23:56:53 +0000

    feat: Create service for getting the user by id

---
+++ backend/src/services/getUserById.ts
@@ -0,0 +1,8 @@
+import { prisma } from '../database/prismaClient.ts';
+
+export default async function getUserById(id: string) {
+	const user = await prisma.user.findUnique({
+		where: { id },
+	});
+	return user;
+}
