066be2f822354a50e0961143a81f8291a411ada0
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-06-10 11:08:12 +0800

    chore: Update frontend linting

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,19 +4,31 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
 	globalIgnores(['dist']),
 	{
-		files: ['**/*.{ts,tsx}'],
+		files: ['src/**/*.{ts,tsx}'],
 		extends: [
 			js.configs.recommended,
-			tseslint.configs.recommended,
+			tseslint.configs.strictTypeChecked,
+			tseslint.configs.stylisticTypeChecked,
 			reactHooks.configs.flat.recommended,
 			reactRefresh.configs.vite,
 		],
 		languageOptions: {
 			globals: globals.browser,
+			parserOptions: {
+				projectService: true,
+			},
+		},
+		rules: {
+			'@typescript-eslint/restrict-template-expressions': [
+				'error',
+				{ allowNumber: true },
+			],
 		},
 	},
+	prettierConfig,
 ]);
